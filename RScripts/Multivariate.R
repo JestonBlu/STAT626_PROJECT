600d58a32cc0c67ac2299751d2adad4c69a25986
JestonBlu <jestonblu@gmail.com>
2016-07-13 19:19:36 -0500

    added multivariate model

--- RScripts/Multivariate.R
+++ RScripts/Multivariate.R
@@ -21,16 +21,6 @@ econ.sa.st = data.frame(
   recession_ind = diff(econ.sa$recession_ind, differences = 2)
 )
 
-econ.sa.lag = ts.intersect(
-  unem_rate_sa = econ.sa.st$unem_rate_sa,
-  industrial_production_sa = lag(econ.sa.st$industrial_production_sa, -2),
-  manufacturers_new_orders_sa = lag(econ.sa.st$manufacturers_new_orders_sa, -2),
-  house_price_sa = lag(econ.sa.st$house_price_sa, -2),
-  construction_spend_sa = lag(econ.sa.st$construction_spend_sa, -2),
-  retail_sales_sa = lag(econ.sa.st$retail_sales_sa, -2),
-  dframe = TRUE
-)
-
 
 Ccf(x = econ.sa.st$unem_rate_sa, y = econ.sa.st$industrial_production_sa, lag.max = 12)
 Ccf(x = econ.sa.st$unem_rate_sa, y = econ.sa.st$manufacturers_new_orders_sa, lag.max = 12)
@@ -38,8 +28,50 @@ Ccf(x = econ.sa.st$unem_rate_sa, y = econ.sa.st$house_price_sa, lag.max = 12)
 Ccf(x = econ.sa.st$unem_rate_sa, y = econ.sa.st$construction_spend_sa, lag.max = 12)
 Ccf(x = econ.sa.st$unem_rate_sa, y = econ.sa.st$retail_sales_sa, lag.max = 12)
 
-mdl1 = sarima(xdata = econ.sa.st$unem_rate_sa, p = 1, d = 0, q = 1)
-mdl2 = sarima(xdata = econ.sa.st$unem_rate_sa, p = 1, d = 0, q = 1, xreg = econ.sa.st[, 2:7])
+lag2.plot(econ.sa.st$unem_rate_sa, econ.sa.st$industrial_production_sa, max.lag = 12)    ## No real lag, setting to 2
+lag2.plot(econ.sa.st$unem_rate_sa, econ.sa.st$manufacturers_new_orders_sa, max.lag = 12) ## Lag 4?
+lag2.plot(econ.sa.st$unem_rate_sa, econ.sa.st$house_price_sa, max.lag = 12)              ## Lag 5?
+lag2.plot(econ.sa.st$unem_rate_sa, econ.sa.st$construction_spend_sa, max.lag = 12)       ## No real lag, setting to 3
+lag2.plot(econ.sa.st$unem_rate_sa, econ.sa.st$retail_sales_sa, max.lag = 12)             ## No real lag, setting to 0
 
-mdl3 = sarima(xdata = econ.sa.lag$unem_rate_sa, p = 1, d = 0, q = 1)
-mdl4 = sarima(xdata = econ.sa.lag$unem_rate_sa, p = 1, d = 0, q = 1, xreg = econ.sa.lag[, 2:6], no.constant = T)
+## Not really going in this direction, but does unemployment lead other metrics?
+lag2.plot(econ.sa.st$industrial_production_sa, econ.sa.st$unem_rate_sa, max.lag = 12)    ## No
+lag2.plot(econ.sa.st$manufacturers_new_orders_sa, econ.sa.st$unem_rate_sa, max.lag = 12) ## No
+lag2.plot(econ.sa.st$house_price_sa, econ.sa.st$unem_rate_sa, max.lag = 12)              ## Leading 8,9?
+lag2.plot(econ.sa.st$construction_spend_sa, econ.sa.st$unem_rate_sa, max.lag = 12)       ## Leading 9-12?
+lag2.plot(econ.sa.st$retail_sales_sa, econ.sa.st$unem_rate_sa, max.lag = 12)             ## Leading 1,6?
+
+
+econ.sa.lag = ts.intersect(
+  unem_rate_sa = econ.sa.st$unem_rate_sa,
+  industrial_production_sa = lag(econ.sa.st$industrial_production_sa, -2),
+  manufacturers_new_orders_sa = lag(econ.sa.st$manufacturers_new_orders_sa, -4),
+  house_price_sa = lag(econ.sa.st$house_price_sa, -5),
+  construction_spend_sa = lag(econ.sa.st$construction_spend_sa, -3),
+  retail_sales_sa = lag(econ.sa.st$retail_sales_sa, -0),
+  dframe = TRUE
+)
+
+
+mdl1 = sarima(xdata = econ.sa.st$unem_rate_sa, p = 1, d = 0, q = 1)                             ## No xRegs
+mdl2 = sarima(xdata = econ.sa.st$unem_rate_sa, p = 1, d = 0, q = 1, xreg = econ.sa.st[, 2:7])   ## No lag xRegs
+mdl3 = sarima(xdata = econ.sa.lag$unem_rate_sa, p = 1, d = 0, q = 1, xreg = econ.sa.lag[, 2:6]) ## Lagged xRegs
+
+#### Model Comparison
+## 
+## Model 1: {AIC: -2.617} {BIC: -3.578} *** Best BIC
+## Model 2: {AIC: -2.613} {BIC: -3.495}
+## Model 3: {AIC: -2.672} {BIC: -3.565} *** Best AIC
+##
+#### Model 3 Pvalues
+##
+##                             Estimate     SE  t.value Pvalue
+## ar1                          -0.2176 0.0672  -3.2387   .001 ***
+## ma1                          -0.8835 0.0411 -21.4938  <.001 ***
+## intercept                     0.0001 0.0009   0.1447   .886
+## industrial_production_sa     -0.0500 0.0132  -3.7763  <.001 ***
+## manufacturers_new_orders_sa  -0.0005 0.0007  -0.6516   .523
+## house_price_sa               -0.0413 0.0122  -3.3765  <.001 ***
+## construction_spend_sa         0.0120 0.0067   1.7902   .091 
+## retail_sales_sa               0.0027 0.0013   2.1645   .044 ***
+
