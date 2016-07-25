693ed918c8c9af2615881664cf60f57bc87701ac
trlilley12 <trlilley12@gmail.com>
2016-07-25 16:34:47 -0500

    Add files via upload

---
+++ RScripts/CCF 7_25_16.R
@@ -0,0 +1,25 @@
+library(xlsx)
+library(astsa)
+library(TSA)
+library(vars)
+library(forecast)
+
+# Load the seasonally adjusted data
+load("C:\\Users\\LILLEYT\\Downloads\\Data_Prep (7).rda")
+names(econ.sa) <- c("unem", "ipi", "orders", "house", "constr", "retail", "recession")
+attach(econ.sa)
+
+fit <- VAR(cbind(unem, constr, retail, recession), type = "both")
+
+un.res <- residuals(fit)[, 1]
+con.res <- residuals(fit)[, 2]
+ret.res <- residuals(fit)[, 3]
+rec.res <- residuals(fit)[, 4]
+
+par(mfrow = c(1, 4))
+
+acf(un.res)
+ccf(un.res, con.res)
+ccf(un.res, ret.res)
+ccf(un.res, rec.res)
+
